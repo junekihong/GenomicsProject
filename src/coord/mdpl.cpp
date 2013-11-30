dc76f03b13be3f31577bbbff26fd705604c172a0
Paul O'Neil <redballoon36@gmail.com>
2013-11-29 19:09:30 -0500

    Catch exceptions on the leader

--- src/coord/mdpl.cpp
+++ src/coord/mdpl.cpp
@@ -65,90 +65,110 @@ int main(int argc, const char* argv[])
 {
     LeaderConfiguration config = parse_options(argc, argv);
     
-    int listen_socket = start_listening(config.myport);
-    std::cout << "Listening on port " << config.myport << "\n";
-    
-    fd_set mask, dummy_mask, temp_mask;
-    
-    FD_ZERO(&mask);
-    FD_ZERO(&dummy_mask);
-    FD_SET(listen_socket, &mask);
-    
-    std::set<int> unannounced_sockets;
-    std::set<NetworkHandler*> handlers;
-    
-    for(;;)
-    {
-        temp_mask = mask;
-        int num = select(FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
-        std::cout << "awoke from select! num = " << num << "\n";
-        if( num > 0 )
+    try {
+        int listen_socket = start_listening(config.myport);
+        std::cout << "Listening on port " << config.myport << "\n";
+        
+        fd_set mask, dummy_mask, temp_mask;
+        
+        FD_ZERO(&mask);
+        FD_ZERO(&dummy_mask);
+        FD_SET(listen_socket, &mask);
+        
+        std::set<int> unannounced_sockets;
+        std::set<NetworkHandler*> handlers;
+        
+        for(;;)
         {
-            if( FD_ISSET(listen_socket, &temp_mask) )
+            temp_mask = mask;
+            int num = select(FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
+            if( num > 0 )
             {
-                int new_socket = accept(listen_socket, 0, 0);
-                FD_SET(new_socket, &mask);
-                unannounced_sockets.insert(new_socket);
-            }
-            
-            std::set<NetworkHandler*> to_erase;
-            for( std::set<NetworkHandler*>::iterator iter = handlers.begin(); iter != handlers.end(); ++iter )
-            {
-                NetworkHandler * cur_handler = *iter;
-                if( FD_ISSET(cur_handler->getSocket(), &temp_mask) )
+                if( FD_ISSET(listen_socket, &temp_mask) )
                 {
-                    bool keep = cur_handler->handleNetwork();
-                    if( !keep )
-                    {
-                        close(cur_handler->getSocket());
-                        to_erase.insert(cur_handler);
-                    }
+                    int new_socket = accept(listen_socket, 0, 0);
+                    FD_SET(new_socket, &mask);
+                    unannounced_sockets.insert(new_socket);
                 }
-            }
-            for( std::set<NetworkHandler*>::iterator iter = to_erase.begin(); iter != to_erase.end(); ++iter )
-            {
-                delete *iter;
-                handlers.erase(*iter);
-            }
-            
-            for( std::set<int>::iterator iter = unannounced_sockets.begin(); iter != unannounced_sockets.end(); ++iter )
-            {
-                int cur_sock = *iter;
-                if( FD_ISSET(cur_sock, &temp_mask) )
+                
+                std::set<NetworkHandler*> to_erase;
+                for( std::set<NetworkHandler*>::iterator iter = handlers.begin(); iter != handlers.end(); ++iter )
                 {
-                    int announce_type;
-                    ssize_t bytes_read = recvfrom(cur_sock, &announce_type, sizeof(announce_type), MSG_WAITALL, NULL, NULL);
-                    if( bytes_read != sizeof(announce_type) )
+                    NetworkHandler * cur_handler = *iter;
+                    if( FD_ISSET(cur_handler->getSocket(), &temp_mask) )
                     {
-                        std::cerr << "Error reading an announcement\n";
-                        close(cur_sock);
-                        FD_CLR(cur_sock, &mask);
-                    }
-                    else
-                    {
-                        switch(announce_type)
+                        bool keep = cur_handler->handleNetwork();
+                        if( !keep )
                         {
-                            case ANNOUNCE_CLIENT:
-                                handlers.insert(new ClientHandler(cur_sock));
-                                std::cout << "Connected a client.\n";
-                                break;
-                            case ANNOUNCE_LEADER:
-                                // Ignore this for now
-                                close(cur_sock);
-                                break;
-                            case ANNOUNCE_WORKER:
-                                handlers.insert(new WorkerHandler(cur_sock));
-                                std::cout << "Connected a worker.\n";
-                                break;
-                            case ANNOUNCE_STORAGE:
-                                // This should probably be a no-op
-                                break;
+                            close(cur_handler->getSocket());
+                            to_erase.insert(cur_handler);
                         }
                     }
-                    unannounced_sockets.erase(cur_sock);
                 }
+                for( std::set<NetworkHandler*>::iterator iter = to_erase.begin(); iter != to_erase.end(); ++iter )
+                {
+                    delete *iter;
+                    handlers.erase(*iter);
+                }
+                
+                std::set<int> sockets_to_erase;
+                for( std::set<int>::iterator iter = unannounced_sockets.begin(); iter != unannounced_sockets.end(); ++iter )
+                {
+                    int cur_sock = *iter;
+                    if( FD_ISSET(cur_sock, &temp_mask) )
+                    {
+                        int announce_type = 0;
+                        ssize_t bytes_read = recvfrom(cur_sock, &announce_type, sizeof(announce_type), MSG_WAITALL, NULL, NULL);
+                        if( bytes_read != sizeof(announce_type) )
+                        {
+                            std::cerr << "Error reading an announcement\n";
+                            close(cur_sock);
+                            FD_CLR(cur_sock, &mask);
+                        }
+                        else
+                        {
+                            switch(announce_type)
+                            {
+                                case ANNOUNCE_CLIENT:
+                                    handlers.insert(new ClientHandler(cur_sock));
+                                    std::cout << "Connected a client.\n";
+                                    break;
+                                case ANNOUNCE_LEADER:
+                                    // Ignore this for now
+                                    close(cur_sock);
+                                    break;
+                                case ANNOUNCE_WORKER:
+                                    handlers.insert(new WorkerHandler(cur_sock));
+                                    std::cout << "Connected a worker.\n";
+                                    break;
+                                case ANNOUNCE_STORAGE:
+                                    // This should probably be a no-op
+                                    break;
+                                default:
+                                    std::cerr << "Unknown announcement: " << announce_type << "\t" <<
+                                        (static_cast<char>(announce_type & 0xff)) << " " <<
+                                        (static_cast<char>((announce_type >> 8) & 0xff)) << " " <<
+                                        (static_cast<char>((announce_type >> 16) & 0xff)) << " " <<
+                                        (static_cast<char>((announce_type >> 24) & 0xff)) << " "
+                                        <<"\n";
+                                    break;
+                            }
+                        }
+                        sockets_to_erase.insert(cur_sock);
+                    }
+                }
+                for( std::set<int>::iterator iter = sockets_to_erase.begin(); iter != sockets_to_erase.end(); ++iter )
+                {
+                    unannounced_sockets.erase(*iter);
+                }
+
             }
         }
+    }
+    catch( const std::exception& err )
+    {
+        std::cerr << err.what() << "\n";
+        exit(-1);
     }
     return 0;
 }
