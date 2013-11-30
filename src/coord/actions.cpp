e9afa012307ca483429ecc3ce7cc3065bf691b53
Paul O'Neil <redballoon36@gmail.com>
2013-11-29 23:06:59 -0500

    Start implementing client actions

--- src/coord/actions.cpp
+++ src/coord/actions.cpp
@@ -7,7 +7,24 @@ WorkerActions * workerActionFactory(LeaderWorkerProtocol * w)
     return NULL;
 }
 
+class ClientActionImpl : public ClientActions
+{
+    LeaderClientProtocol * client;
+    public:
+    ClientActionImpl(LeaderClientProtocol * c);
+    
+    virtual void startGenomeUpload(const std::string& name, unsigned length);
+    virtual void continueGenomeUpload(const std::vector<char>& data);
+    virtual void listGenomes();
+    virtual void alignmentRequest(const std::string& first, const std::string& second);
+};
+
 ClientActions * clientActionFactory(LeaderClientProtocol * c)
 {
-    return NULL;
+    return new ClientActionImpl(c);
+}
+
+void ClientActionImpl::startGenomeUpload(const std::string &name, unsigned int length)
+{
+    //TODO contact storage
 }
