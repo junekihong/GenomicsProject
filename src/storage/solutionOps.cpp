1a75fa375dcaf7d99d66a459cc03e61ce498122e
Paul O'Neil <redballoon36@gmail.com>
2013-12-01 03:03:48 -0500

    Find solutions by matching exactly on the problem description

--- src/storage/solutionOps.cpp
+++ src/storage/solutionOps.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <set>
 
@@ -60,6 +62,8 @@ std::set<const CompleteSolution*, bool(*)(const CompleteSolution*, const Complet
 std::set<const CompleteSolution*, bool(*)(const CompleteSolution*, const CompleteSolution*)> leftNumberIndex(compare_left_numbers);
 std::set<const CompleteSolution*, bool(*)(const CompleteSolution*, const CompleteSolution*)> topGenomeIndex(compare_top_genomes);
 std::set<const CompleteSolution*, bool(*)(const CompleteSolution*, const CompleteSolution*)> leftGenomeIndex(compare_left_genomes);
+
+typedef std::set<const CompleteSolution*, bool(*)(const CompleteSolution*, const CompleteSolution*)>::iterator IndexIter;
 
 void initializeSolutionSystem()
 {
@@ -135,7 +139,44 @@ void queryByID(ProblemID id, bool solution_wanted, QueryResponse& resp)
     }
 }
 
+static void queryExactly(const ProblemDescription& desc, QueryResponse& resp);
+static void queryApproximately(const ProblemDescription& desc, QueryResponse& resp);
+
 void queryByConditions(const ProblemDescription& desc, bool partialsWanted, QueryResponse& resp)
 {
+    if( !partialsWanted ) {
+        queryExactly(desc, resp);
+        return;
+    }
+    else {
+        queryApproximately(desc, resp);
+    }
+}
+
+static void queryExactly(const ProblemDescription& desc, QueryResponse& resp)
+{
+    CompleteSolution dummySolution;
+    dummySolution.desc = desc;
+    std::pair<IndexIter, IndexIter> top_num_range = topNumberIndex.equal_range(&dummySolution);
+    std::pair<IndexIter, IndexIter> left_num_range = leftNumberIndex.equal_range(&dummySolution);
+    std::pair<IndexIter, IndexIter> top_gen_range = topGenomeIndex.equal_range(&dummySolution);
+    std::pair<IndexIter, IndexIter> left_gen_range = leftGenomeIndex.equal_range(&dummySolution);
     
+    std::set<const CompleteSolution*> matches[2];
+    std::set_intersection(top_num_range.first, top_num_range.second, left_num_range.first, left_num_range.second, std::inserter(matches[0], matches[0].begin()));
+    std::set_intersection(top_gen_range.first, top_gen_range.second, left_gen_range.first, left_gen_range.second, std::inserter(matches[1], matches[1].begin()));
+    
+    std::set<const CompleteSolution*> result;
+    std::set_intersection(matches[0].begin(), matches[0].end(), matches[1].begin(), matches[1].end(), std::inserter(result, result.begin()));
+    
+    if( result.size() )
+    {
+        const CompleteSolution * answer = *result.begin();
+        resp.success = true;
+        resp.exactMatch = true;
+        resp.problemDescription = answer->desc;
+        resp.maxValue = answer->sol.maxValue;
+        resp.location = answer->sol.maxValueLocation;
+        resp.sol = new Solution(answer->sol);
+    }
 }
