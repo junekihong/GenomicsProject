2f1d67c116a6ddc5b29ada5fe14622bdfe4f5a82
Paul O'Neil <redballoon36@gmail.com>
2013-12-06 19:03:37 -0500

    Add connect function for int (non-boost) sockets

--- src/common/connect.cpp
+++ src/common/connect.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <boost/asio/connect.hpp>
 
 #include "connect.h"
@@ -24,4 +28,32 @@ void connect_server(tcp::iostream& socket, const ServerEndpoint& server, const s
         std::cerr << "Aborting!\n";
         exit(-1);
     }
+}
+
+int start_connection(const char* host, int port)
+{
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if( s < 0 ) {
+        std::cerr << "Error creating socket\nAborting\n";
+        exit(-1);
+    }
+    
+    struct hostent * remote_host = gethostbyname(host);
+    int host_num;
+    if( remote_host == NULL ) {
+        std::cerr << "Error finding hostname\nAborting\n";
+        exit(-1);
+    }
+    memcpy(&host_num, remote_host->h_addr_list[0], sizeof(host_num));
+    
+    struct sockaddr_in send_addr;
+    send_addr.sin_family = AF_INET;
+    send_addr.sin_addr.s_addr = host_num;
+    send_addr.sin_port = htons(port);
+    int ret = connect(s, (struct sockaddr*)&send_addr, sizeof(send_addr));
+    if( ret < 0 ) {
+        std::cerr << "error connecting\n";
+        exit(-1);
+    }
+    return s;
 }
