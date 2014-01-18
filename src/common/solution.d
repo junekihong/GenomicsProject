5f5f6610b1f4271ec9e9808193d337ea3b9932a9
Paul O'Neil <redballoon36@gmail.com>
2014-01-18 17:49:47 -0500

    Add immutable / shared dup, opAssign to Solution

--- src/common/solution.d
+++ src/common/solution.d
@@ -17,4 +17,28 @@ struct Solution
     int maxValue;
     Location maxValueLocation;
     Matrix matrix;
+
+    @property Solution dup() inout
+    {
+        Solution result;
+        result.id = id;
+        result.maxValue = maxValue;
+        result.maxValueLocation = maxValueLocation;
+        result.matrix = matrix; // Matrix assignment does a deep copy
+        return result;
+    }
+
+    @property immutable(Solution) idup()
+    {
+        return immutable Solution(
+            id, maxValue, maxValueLocation, matrix.idup);
+    }
+
+    void opAssign(in Solution other) shared
+    {
+        id = other.id;
+        maxValue = other.maxValue;
+        maxValueLocation = other.maxValueLocation;
+        matrix = other.matrix;
+    }
 }
